87d4c2b71d4f49b74a3c88be51c4f987d3b78431
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-03 19:17:52 -0700

    version 2
    trying to add hooks to page one

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,13 @@
 
 
+  //hooks into page one title
+  $('#page1 h2').text("zackary");
 
+  //hooks into page one temp
+  $('#page1 b').text("this is the temp");
 
-
-
-
-if (navigator.geolocation) {
-    // Yes! Show button
-    $('.getGeolocation').show(); 
-  } else {
-    // No. Hide button
-    $('.getGeolocation').hide();
-  }
-
-$('.getGeolocation').on('click', function() {
-  
-    navigator.geolocation.getCurrentPosition(function(position) {
-    //load weather using your lat/lng coordinates. See _loadWeather()_ below
-    loadWeather(position.coords.latitude+','+position.coords.longitude); 
-    // See latitute & longitude. Note, wait a few seconds
-    console.log(position.coords.latitude+','+position.coords.longitude);
-  });
-  
-});
+  //hooks into page one image
+  $('#page1 div.weather-image').text("image");
  
   
 
