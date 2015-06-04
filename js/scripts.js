b40365d1c92a482b4ba1bb84a9d7c7f281a0c09a
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-03 19:58:13 -0700

    3

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
+        $('#page1 h2').text("zackary");
 
+        //hooks into page one temp
+        $('#page1 b').text("this is the temp");
+
+        //hooks into page one image
+        $('#page1 div.weather-image').text("image");
 
 
 
