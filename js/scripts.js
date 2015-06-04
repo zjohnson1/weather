ab5733f0c9e5c1e7fa309d7a5776db3bed9a3040
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-04 11:52:16 -0700

    5

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,29 @@ if(navigator.geolocation){
     // See latitute & longitude. Note, wait a few seconds
     console.log(location);
 
-  }
+  };
 }  
 
 //weather function
-$.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
+  var loadWeather = function(location){
+    $.simpleWeather({
+      location: 'Austin, TX',
+      woeid: '',
+      unit: 'f',
+      success: function(weather) {
+          
+          $('#page1 h2').text(weather.city + ', ' + weather.region);
+
+          //hooks into page one temp
+          $('#page1 b').text(weather.temp + weather.units.temp);
+
+          //hooks into page one image
+          $('#page1 div.weather-image').text("image");
+    
         
-        $('#page1 h2').text(weather.city + ', ' + weather.region);
-
-        //hooks into page one temp
-        $('#page1 b').text(weather.temp + weather.units.temp);
-
-        //hooks into page one image
-        $('#page1 div.weather-image').text("image");
-  
-      
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-});
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+  });
+{}
