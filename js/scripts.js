fb2cd19dd728a6e52e29d76cac9950f27bc055a1
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-04 08:42:04 -0700

    version 5
    fixing function call

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,7 @@
 
-$('#page1 .home. getGeolocation').on('load', function() {
-  
-    navigator.geolocation.getCurrentPosition(function(position) {
-    //load weather using your lat/lng coordinates. See _loadWeather()_ below
-    loadWeather(position.coords.latitude+','+position.coords.longitude); 
-    // See latitute & longitude. Note, wait a few seconds
-    console.log(position.coords.latitude+','+position.coords.longitude);
-  });
-  
-});
 
-
-var loadWeather = function(location){
+//weather function
+var loadWeather = function(){
   $.simpleWeather({
     location: 'Austin, TX',
     woeid: '',
