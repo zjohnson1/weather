344ff1d3a89b53b45678bfedd7d7f1e254e2605f
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-04 13:02:24 -0700

    version 6

--- js/scripts.js
+++ js/scripts.js
@@ -29,11 +29,25 @@ if(navigator.geolocation){
           //hooks into page one image
           $('#page1 div.weather-image').text("image");
 
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+  });
+  $.simpleWeather({
+      location: 'Seattle WA',
+      woeid: '',
+      unit: 'f',
+      success: function(weather) {
+          
+          $('#page3 h2').text(weather.city + ', ' + weather.region);
+
+          //hooks into page one temp
           $('#page3 b').text(weather.temp + weather.units.temp);
 
-          
-    
-        
+          //hooks into page one image
+          $('#page3 div.weather-image').text("image");
+
       },
       error: function(error) {
         $("#weather").html('<p>'+error+'</p>');
