9eee8fc9e540a28741fe9993ac20fc31d4c3e08d
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-03 20:06:28 -0700

    3

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,22 @@
-        $('#page1 h2').text("zackary");
+
+
+
+$.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+              $('#page1 h2').text("zackary");
 
         //hooks into page one temp
         $('#page1 b').text("this is the temp");
 
         //hooks into page one image
         $('#page1 div.weather-image').text("image");
-
-
-
-
- var loadWeather = function() { 
-  $.simpleWeather({
-
-      //create function called testResults which take in a div takes the form in the div
-      //and assigns the value wich should be a number zip code to the variable zip
-      /*function testResults (div) {
-
-        var zip = div.input.value;
-      
-      }*/
-      
-
-      
-      location: 'Austin, TX',
-      
-      
-      success: function(weather) {
-        //hide all of my icons
-        
-
-        //hooks into page one title
-        $('#page1 h2').text("zackary");
-
-        //hooks into page one temp
-        $('#page1 b').text("this is the temp");
-
-        //hooks into page one image
-        $('#page1 div.weather-image').text("image");
-
-        
-
-        console.log("made it");
-
-      },
-      
-      error: function(error) {
-        $("#weather").html('<p>'+error+'</p>');
-      }
-      
-    });
-  };
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
