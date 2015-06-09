9f646b4f93ad3f477b11dc57e554856112c882bf
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-08 21:11:19 -0700

    image

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ if(navigator.geolocation){
 
 
           //hooks into page one image
-          $('#page1 div.weather-image').append("<img id='theImg' style='height: 100%; width: 100%; position:absolute' src='"+weather.image+"'/>");
+          $('#page1 div.weather-image').append("<img id='theImg' style='height: 100%; width: 100%; position:absolute; left: 40px; top: 40px' src='"+weather.image+"'/>");
 
       },
       error: function(error) {
