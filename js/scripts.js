11fb7f57f8a3ff2367640b554d2ae24ea570de19
zjohnson1 <zjohnson@eagles.ewu.edu>
2015-06-09 20:32:17 -0700

    check

--- js/scripts.js
+++ js/scripts.js
@@ -91,7 +91,7 @@ if(navigator.geolocation){
 
           //$('.five-day').remove();
           $('#page7 .five-day').append("<ul><li><span style='font-family: georgia, palatino;'>"+weather.forecast[0].day+"</span><ul><li>"+weather.forecast[0].text + ' High ' + weather.forecast[0].high + ', Low ' + weather.forecast[0].low+"</li></ul></li><li><span style='font-family: georgia, palatino;'>"+weather.forecast[1].day+"</span><ul><li>"+weather.forecast[1].text + ' High ' + weather.forecast[1].high + ', Low ' + weather.forecast[1].low+"</li></ul></li><li><span style='font-family: georgia, palatino;'>"+weather.forecast[2].day+"</span><ul><li>"+weather.forecast[2].text + ' High ' + weather.forecast[2].high + ', Low ' + weather.forecast[2].low+"</li></ul></li><li><span style='font-family: georgia, palatino;'>"+weather.forecast[3].day+"</span><ul><li>"+weather.forecast[3].text + ' High ' + weather.forecast[3].high + ', Low ' + weather.forecast[3].low+"</li></ul></li><li><span style='font-family: georgia, palatino;'>"+weather.forecast[4].day+"</span><ul><li>"+weather.forecast[4].text + ' High ' + weather.forecast[4].high + ', Low ' + weather.forecast[4].low+"</li></ul></li></ul>");
-
+          console.log(wind.speed);
           $('#page7 h3.wind').text("Wind "+wind.speed+ units.speed + " "+ wind.direction);
 
       },
